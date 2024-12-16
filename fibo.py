d40b04a136a5eafa27aeb794d8eb60a7c86bedf5
haeul <dlgodnf99@naver.com>
2024-12-16 11:50:56 +0900

    feat: Update fibo.py

--- fibo.py
+++ fibo.py
@@ -6,6 +6,6 @@
 from math import sqrt
 def fibo(num):
     phi = (1 + math.sqrt(5)) / 2
-    return round(pow(phi, n) / math.sqrt(5))
+    return round(pow(phi, num) / math.sqrt(5))
 
 print(fibo(5))
