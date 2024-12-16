c922efd1cd258ad6462ec3fe2f631e5e1f5e08f9
haeul <dlgodnf99@naver.com>
2024-12-16 12:01:06 +0900

    feat: Update fibo.py to binet formula

--- fibo.py
+++ fibo.py
@@ -1,7 +1,11 @@
+#def fibo(num):
+#    if num < 2:
+#        return num
+#    else:
+#        return fibo(num - 1) + fibo(num - 2)
+from math import sqrt
 def fibo(num):
-    if num < 2:
-        return num
-    else:
-        return fibo(num - 1) + fibo(num - 2)
+    phi = (1 + math.sqrt(5)) / 2
+    return round(pow(phi, n) / math.sqrt(5))
 
 print(fibo(5))
